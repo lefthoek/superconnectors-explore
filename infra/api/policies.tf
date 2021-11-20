a7bb893970dff873cbeb49bc8101c0bcc01d20a4
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-11-20 22:03:55 +0100

    update role

--- infra/api/policies.tf
+++ infra/api/policies.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "api_gateway_access" {
   name = "${var.project_prefix}-${local.name}-api-gateway-access-policy-${var.environment_name}"
   path   = "/"
-  policy = data.aws_iam_policy_document.api_gateway_access
+  policy = data.aws_iam_policy_document.api_gateway_access.json
 }
