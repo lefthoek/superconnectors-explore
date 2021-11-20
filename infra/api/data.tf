2db11cad6dc88e4bfba05d52f9d9e0a5696c96a9
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-11-20 22:17:18 +0100

    update role

--- infra/api/data.tf
+++ infra/api/data.tf
@@ -1,3 +1,5 @@
+data "aws_region" "current" {}
+
 data "aws_iam_policy_document" "lambda_role_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -14,8 +16,7 @@ data "aws_iam_policy_document" "api_gateway_access" {
     actions = ["execute-api:*"]
 
     resources = [
-        "arn:aws:execute-api:*"
-
+      "arn:aws:execute-api:*:*:*"
     ]
   }
 }
