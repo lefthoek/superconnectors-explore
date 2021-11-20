a61dd2e79118a13fce19dba648798c603b95dcce
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-11-20 23:38:59 +0100

    add stage

--- infra/websockets/api.tf
+++ infra/websockets/api.tf
@@ -4,18 +4,8 @@ resource "aws_apigatewayv2_api" "ws" {
   route_selection_expression = "$request.body.action"
 }
 
-resource "aws_apigatewayv2_deployment" "ws" {
-  api_id      = aws_apigatewayv2_route.ws.id
-  description = "Example deployment"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_apigatewayv2_stage" "ws" {
   api_id        = aws_apigatewayv2_api.ws.id
   name          = var.environment_name
-  deployment_id = aws_apigatewayv2_deployment.ws.id
 }
 
