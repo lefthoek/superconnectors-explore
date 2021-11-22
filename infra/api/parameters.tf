b9c0152a8289b6f52c5b6ef459b78629c424fbc9
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-11-22 22:25:59 +0100

    add arn to outputs

--- infra/api/parameters.tf
+++ infra/api/parameters.tf
@@ -4,16 +4,28 @@ resource "aws_ssm_parameter" "role" {
   value       = aws_iam_role.api_role.arn
 }
 
-resource "aws_ssm_parameter" "connections_table" {
-  name        = "${local.parameter_prefix}/connections_table"
+resource "aws_ssm_parameter" "connections_table_name" {
+  name        = "${local.parameter_prefix}/connections_table/name"
   type        = "SecureString"
   value       = module.websockets.connections_table_name
 }
 
 resource "aws_ssm_parameter" "subscriptions_table" {
-  name        = "${local.parameter_prefix}/subscriptions_table"
+  name        = "${local.parameter_prefix}/subscriptions_table/name"
   type        = "SecureString"
   value       = module.websockets.subscriptions_table_name
+}
+
+resource "aws_ssm_parameter" "connections_arn" {
+  name        = "${local.parameter_prefix}/connections_table/arn"
+  type        = "SecureString"
+  value       = module.websockets.connections_table_arn
+}
+
+resource "aws_ssm_parameter" "subscriptions_arn" {
+  name        = "${local.parameter_prefix}/subscriptions_table/arn"
+  type        = "SecureString"
+  value       = module.websockets.subscriptions_table_arn
 }
 
 resource "aws_ssm_parameter" "apigateway_id" {
